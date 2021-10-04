3d47eb6214d14df664b848369aef5522ceaf05ca
xTheCoderx <justaguyoney@gmail.com>
2021-10-04 15:26:34 +1300

    Added option to input values from console.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b, c, d;
+
+    cout << "Input a value for 'a': " << endl;
+    cin >> a;
+
+    cout << "Input a value for 'b': " << endl;
+    cin >> b;
+
+    cout << "Input a value for 'c': " << endl;
+    cin >> c;
+
+    cout << "Input a value for 'd': " << endl;
+    cin >> d;
+
     return 0;
 }
